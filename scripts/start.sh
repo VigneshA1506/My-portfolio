af5ba4b34852a62bcc592bb02a290261744a8881
VigneshA1506 <vikkiamav@gmail.com>
2026-04-29 23:26:38 +0530

    changes in buildspec.yml

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  
 echo "Pulling latest Docker image..."
-docker pull vickyamav/first-repo:latest
+sudo docker pull vickyamav/first-repo:latest
  
 echo "Stopping old container..."
-docker stop my-react-container || true
+sudo docker stop my-react-container || true
  
 echo "Removing old container..."
-docker rm my-react-container || true
+sudo docker rm my-react-container || true
  
 echo "Running new container..."
-docker run -d -p 80:80 --name my-react-container vickyamav/first-repo:latest
+sudo docker run -d -p 80:80 --name my-react-container vickyamav/first-repo:latest
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 echo "Stopping existing container..."
-docker stop my-react-container || true
+sudo docker stop my-react-container || true
  
 echo "Removing existing container..."
-docker rm my-react-container || true
+sudo docker rm my-react-container || true

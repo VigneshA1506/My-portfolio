7b995252ebfcb8a0132eeee677e5d207c19acc41
VigneshA1506 <vikkiamav@gmail.com>
2026-04-29 12:20:24 +0530

    changes

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,4 +4,4 @@ echo "Stopping existing container..."
 docker stop my-react-container || true
  
 echo "Removing existing container..."
-docker rm my-react-container || true
+docker rm my-react-container || true 
